bc44b9edc4f1789aef31c09e7eee7f760d4e99c8
mjainta <martin.jainta@googlemail.com>
2021-08-08 10:47:31 +0200

    :sparkles: express: Connect to mongo

--- expressjs/routes/documents.js
+++ expressjs/routes/documents.js
@@ -1,9 +1,21 @@
+const { MongoClient } = require('mongodb')
+
+const url = 'mongodb://admin:foobar@localhost'
+const client = new MongoClient(url)
+const dbName = 'mydatabase'
+
 var express = require('express');
 var router = express.Router();
 
 /* GET document listings. */
-router.get('/', function(req, res, next) {
-  res.send('sdfsdf respond with a resource');
+router.get('/', async function(req, res, next) {
+  await client.connect()
+  console.log('Connected successfully to server')
+  const db = client.db(dbName)
+  const collection = db.collection('mycoll')
+  const findResult = await collection.find({}).toArray()
+
+  res.send(findResult);
 });
 
 module.exports = router;
